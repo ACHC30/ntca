2155956c6916e3e07f1ff6ba126bfc2009d8e02c
achc30 <chandrachristopher43@gmail.com>
2024-02-08 19:12:02 +1000

    geocode update

--- src/components/MapWithPin.js
+++ src/components/MapWithPin.js
@@ -8,11 +8,33 @@ const MapWithPin = () => {
   });
 
   const [position, setPosition] = useState({ lat: 51.505, lng: -0.09 });
+  const [address, setAddress] = useState("");
 
   const handleMarkerDrag = (event) => {
     setPosition({
       lat: event.latLng.lat(),
       lng: event.latLng.lng()
+    });
+    findAddress({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    })
+  };
+
+  const findAddress = (event) => {
+    const geocoder = new window.google.maps.Geocoder();
+    const latlng = { lat: event.lat, lng: event.lng };
+
+    geocoder.geocode({ 'location': latlng }, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          setAddress(results[0].formatted_address);
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
     });
   };
 
@@ -31,6 +53,9 @@ const MapWithPin = () => {
           onDragEnd={handleMarkerDrag}
         />
       </GoogleMap>
+      <div style={{ background: 'white', color:"black",position: 'absolute', top: '10px', left: '10px', padding: '10px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)'}}>
+        <strong>Address:</strong> {address}
+      </div>
     </div>
   ) : null;
 };
